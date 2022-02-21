e605d01fceaef9e15dedb36869a9276a14031ba2
ramses5300 <85216669+ramses5300@users.noreply.github.com>
2022-02-22 00:34:09 +0900

    Create office_ms_test01.py

---
+++ office_ms_test01.py
@@ -0,0 +1,15 @@
+import msal
+from office365.graph_client import GraphClient
+
+def acquire_token():
+    """
+    Acquire token via MSAL
+    """
+    authority_url = 'https://login.microsoftonline.com/{tenant_id_or_name}'
+    app = msal.ConfidentialClientApplication(
+        authority=authority_url,
+        client_id='{client_id}',
+        client_credential='{client_secret}'
+    )
+    token = app.acquire_token_for_client(scopes=["https://graph.microsoft.com/.default"])
+    return token
